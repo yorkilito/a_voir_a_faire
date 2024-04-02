2b7d5c8c2f78496606a6a03f9a3b13add916cb5f
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2024-04-02 15:14:20 +0200

    update .env file

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ require_once("model/CommentStorageMySQL.php");
 //$file = "database.txt";
 //$db = new ObjectFileDB($file);
 //$pdo= new PDO("mysql:host=".Config::MYSQL_HOST.";port=".Config::MYSQL_PORT.";dbname=".Config::MYSQL_DB.";charset=utf8mb4", Config::MYSQL_USER , Config::MYSQL_PASSWORD);
-$pdo= new PDO("mysql:host=".$env["MYSQLHOST"].";port=".$env["MYSQLPORT"].";dbname=".$env["MYSQLDATABASE"].";charset=utf8mb4", $env["MYSQLUSER"] , $env["MYSQLPASSWORD"]);
+$pdo= new PDO("mysql:host=".$_ENV["MYSQLHOST"].";port=".$_ENV["MYSQLPORT"].";dbname=".$_ENV["MYSQLDATABASE"].";charset=utf8mb4", $_ENV["MYSQLUSER"] , $_ENV["MYSQLPASSWORD"]);
 
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
