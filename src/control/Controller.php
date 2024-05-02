8dd87834335d220677678e28be00d586425fbcc2
Samuel Yorke Aidoo <sya.aidoo@gmail.com>
2024-05-02 16:20:43 +0200

    Fix var_dump comment

--- src/control/Controller.php
+++ src/control/Controller.php
@@ -84,7 +84,6 @@
         }
 
         public function saveNewComment(array $data){
-            var_dump($data);
             $this->currentComment = new CommentBuilder($data);
             if($this->currentComment->isValid()){
                 $comment = $this->currentComment->createComment();
